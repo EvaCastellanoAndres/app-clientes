6c914f198b5bb0bbb98eca978ef28a76b491cafa
Maria Lozano Ovejero <mlozanoo@hiberus.com>
2025-02-06 12:30:03 +0100

    validados nombre y edad

--- Practicas/practicas/src/app/crear/crear.component.ts
+++ Practicas/practicas/src/app/crear/crear.component.ts
@@ -1,25 +1,26 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 
 import { VentanaConfirmarComponent } from '../ventana-confirmar/ventana-confirmar.component';
 
 @Component({
   selector: 'app-crear',
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './crear.component.html',
   styleUrl: './crear.component.scss'
 })
 export class CrearComponent {
   formularioCliente = new FormGroup ({
     clienteCodigo: new FormControl(),
-    clienteNombre: new FormControl(),
+    clienteNombre: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-ZáÁéÉíÍóÓúÚ ]+$/)]),
     clienteApellido1: new FormControl(),
     clienteApellido2: new FormControl(),
     clienteDNI: new FormControl(),
     clientePasaporte: new FormControl(),
-    clienteFechaNacimiento: new FormControl(),
+    clienteFechaNacimiento: new FormControl('', [Validators.required, this.validarEdad]),
     clienteCalle: new FormControl(),
     clientePortal: new FormControl(),
     clienteNumero: new FormControl(),
@@ -28,6 +29,7 @@ export class CrearComponent {
     clienteCodigoPostal: new FormControl(),
     clienteCiudad: new FormControl(),
     clienteProvincia: new FormControl(),
+    clienteImagenes: new FormControl()
   })
 
   codigo: number = 0;
@@ -47,6 +49,7 @@ export class CrearComponent {
   constructor(public confirma: MatDialog) {}
 
   abrirConfirmacion () {
+    if (this.formularioCliente.valid) {
       this.confirma.open(VentanaConfirmarComponent, { 
       data: {codigo: this.codigo,
              nombre: this.nombre,
@@ -62,6 +65,28 @@ export class CrearComponent {
              ciudad: this.ciudad,
              provincia: this.provincia
             }
-    });
+      });
+    }
   }
+
+  validarEdad(control: any) {
+    const fechaNac = new Date(control.value);
+    const edad = new Date().getFullYear() - fechaNac.getFullYear();
+    return edad > 17 && edad < 61 ? null : { invalidaAge: true };
+  }
+  /*imagenesSeleccionadas: File[] = [];
+  limiteExcedido:boolean = false;
+
+  onFileChange(event: any): void {
+    const imagenes = event.target.clienteImagenes;
+
+    if (imagenes.length > 4) {
+      this.limiteExcedido = true;
+      return;
+    }
+
+    this.limiteExcedido = false;
+    this.imagenesSeleccionadas = Array.from(imagenes);
+  }*/
+
 }
