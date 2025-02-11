57c2b8c17a12cd6324692a00bd67739af2ffb0b0
Eva Castellano Andres <ecastellano@hiberus.com>
2025-02-11 09:08:36 +0100

    conectar con el front

--- src/main.ts
+++ src/main.ts
@@ -6,23 +6,10 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors({ origin: '*' });
+
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
-
-
-
-
-
-/*import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import {ValidationPipe} from '@nestjs/common';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
-*/
