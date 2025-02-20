7a1895c043a9fad4d89e725d7d73f5ca43495beb
Eva Castellano Andres <ecastellano@hiberus.com>
2025-02-20 10:43:47 +0100

    axios

--- src/cloudinary/cloudinary.controller.ts
+++ src/cloudinary/cloudinary.controller.ts
@@ -1,4 +1,32 @@
-import {
+import { Controller, Post, Body, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import axios from 'axios';
+
+@Controller('cloudinary')
+export class CloudinaryController {
+  @Post('upload')
+  async uploadToCloudinary(
+    @Body() body: any,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    try {
+      const cloudinaryUrl = 'https://api.cloudinary.com/dmhemvly5/image/upload';
+      const response = await axios.post(cloudinaryUrl, body, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      res.status(response.status).json(response.data);
+    } catch (error) {
+      res
+        .status(error.response?.status || 500)
+        .json(error.response?.data || { message: 'Internal Server Error' });
+    }
+  }
+}
+
+/*import {
   Controller,
   Post,
   UseInterceptors,
@@ -18,3 +46,4 @@ export class CloudinaryController {
     return { imageUrl: uploadedImage.secure_url };
   }
 }
+*/
