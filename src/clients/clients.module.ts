e571ee43b1133b4a56e0bebe37bc1fe8b0eaeaf1
Eva Castellano Andres <ecastellano@hiberus.com>
2025-02-21 10:09:11 +0100

    prueba chat3.2

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { ClientsController } from './clients.controller';
 import { UploadModule } from '../upload/upload.module';
+import { CloudinaryService } from '../cloudinary/cloudinary.service';
 
 @Module({
   imports: [UploadModule],
   controllers: [ClientsController],
-  providers: [ClientsService],
+  providers: [ClientsService, CloudinaryService],
 })
 export class ClientsModule {}
